2ad8fe0b46961f3b5a3aa3ed3041d489093a1768
Gratcy <gratcypalma@gmail.com>
2017-11-08 02:44:00 +0700

    term

---
+++ term.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Rock Trading Term &amp; Conditions</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+  </script>
+</head>
+<body>
+<h1>Rock Trading Term &amp; Conditions</h1>
+<div id="tabs">
+  <ul>
+    <li><a href="#tabs-1">Hongkong</a></li>
+    <li><a href="#tabs-2">English</a></li>
+  </ul>
+  <div id="tabs-1">
+<p><b>磐 石  產品保用証</b></p>
+<p>茲保用証背面所列之磐石產品或代理之產品在製造商 出貨前已符合嚴格品質測試，並對該產品之原買主提供 壹年內更換一次服務，條件如下： </p>
+<p>(一)請於購買日起計十天內寄回此証或網上登記。</p>
+<p>(二)於保養期內尋求維修必需出示保用証及購買單據，單據上必需列出分銷造名稱，型號, 機身及購買日期。</p>
+
+
+<p>(三)由購買產品日期壹年內，若機件在正常使用下有任何 損壞或故障，本公司將免費修理更換零件。</p>
+
+<p>(四)本保用証不適用於以下述情況：
+    i 曾經由非本公司技術員修理或更改所造成之損壞。
+    ii 因輸入不當電壓， 錯誤操作，疏忽使用，意外
+    iii 此項免費維修服務不包括機身外殼，電線。</p>
+
+<p>(五)若產品需要換，客戶需負責將產品送往本公司維修
+      中心維修。</p>
+
+<p>(六)請妥存此保用証，並於更換時與正式購買單一併出
+示方獲得免費服務。本証不會補發。</p>
+
+<p>(七)產品機身編號與保用証不符，乃不獲免費維修服務</p>
+
+<p>&nbsp;</p>
+<div style="font-weight: bold;
+font-style: italic;">
+<p>重要事項</p>
+<ul>
+<li><p>為保障保用期內之免費服務，請將發票及此保用証書 妥為保存。</p></li>
+<li><p>以上所載，磐石貿易有限公司有權修改，取消或增加規例。怒不另行同知。</p></li>
+</ul>
+</div>
+<br />
+<p>客户服务号码: +(852)25634322</p>
+  </div>
+  <div id="tabs-2">
+<p><b>Rock Product Guarantee Certificate</b></p>
+
+<p>We   here  by  guarantee   that  the  Rock's   brand  name  or   under  Rock's  representative 
+brand   name  had   identified   overleaf  is  free  from  defects  in material  and  workmanship
+by   manufacture   and   hence  it  is provided  with  free  first  year once replacement service
+within the limit of the following conditions:</p>
+<ol>
+<li><p>Please   mail  back  or  register  on  wesite   within  10  days  from the  day  of  purchase.</p></li>
+<li><p>Proof   of   purchase  shall   be  produced  for  once   replacement   service. Such proof  of
+    purchase  must   be   an  original   invoice  containing   the  name  of   dealer,   the  model
+    serial number and   the  date  of   purchase.</p></li>
+<li><p>For  any  defect,  in   the  judgement  of  our  technician,  caused  under  normal  use,  we
+    shall  repair / replace   free of  charge   with   in   one  year  guarantee  period  commencing
+    from the date delivery / original purchase.</p></li>
+<li><p>This   warranty  will  be  voided   if   the  product  has  been i) repaired,  altered,  tempered
+    by any person other than the technician or service agent of the Company or ii) damaged by
+    input    of    improper    voltage,    misuse,   negligence,   accident   of  natural   calamities. 
+    iii) Replacement of cover panels, wires are to be charged at cost.
+</p></li>
+<li><p>In   case   workshop   service   is   necessary,   the  customer   will   be  responsible  for 
+   delivering the products to our maintenance centre.</p></li>
+<li><p>Please   retain   this  guarantee  certificate  and  present  it  together  with   the  purchase
+    invoice  to  our  technician   for   inspection   when  service  is   rendered.  This  certificate
+    will not re-issued.</p></li>
+<li><p>If  product  serial number mismatch  with  its  guarantee  certificate,  free  first  year  on-site 
+    service will not be provided.</p></li>
+</ol>
+<p>&nbsp;</p>
+<div style="font-weight: bold;
+font-style: italic;">
+<p>Notes: </p>
+<ul>
+<li>Important notice for free once replacement service.</li>
+<li>Please keep this warranty card and the invoice in a safe place.</li>
+<li>Rock  Trading  Company  Limited  reserves  the  right  to  amed,  at  its sole direction and
+    without notice any of the condition described in this warranty. Any liability which otherwise
+    may arise out of or in connection with such alternation is expressly excluded.</li>
+</ul>
+<br />
+<p>Customer Service Number: +(852)25634322</p>
+</div>
+  </div>
+</div>
+
+</body>
+</html>
