a25a8d77fd6fe5e201b17615d8dacb518196526a
Gratcy <gratcypalma@gmail.com>
2018-06-20 21:47:03 +0700

    fixed style add item invoice

--- invoice_edit_item_add.php
+++ invoice_edit_item_add.php
@@ -31,13 +31,13 @@ if (!empty($id)) {
 				<input type=hidden name='cms_item::$rand_id::item_id::new'	value='$id'>
 				<input type=hidden name='cms_item::$rand_id::item_brand::new'	value='$item_info->brand'>
 				<input type=hidden name='cms_item::$rand_id::rec_id::new'		value='$rec_id'>
-				<td style='width:15%'>$item_info->item_id</td>
-				<td style='width:40%;white-space:normal'>$item_info->name</td>
-				<td style='width:10%;min-width:90px'><input class=form-control number type=text name='cms_item::$rand_id::quantity::new'			value='1'							size=2 nextinput=cms_item::$rand_id::price::new		onblur='calculate_item(\"$rand_id\", \"::new\");'></td>
-				<td style='width:10%;min-width:90px''><input class=form-control number type=text name='cms_item::$rand_id::price_original::new'		value='$item_info->price'			size=2 nextinput=cms_item::$rand_id::price::new></td>
-				<td style='width:10%;min-width:90px''><input class=form-control number type=text name='cms_item::$rand_id::price::new'				value='$item_info->price'			size=2 nextinput=input_pulldownmenu_add_item_id		onblur='calculate_item(\"$rand_id\", \"::new\");'></td>
-				<td style='width:10%;min-width:90px''><input class=form-control number type=text name='cms_item::$rand_id::amount::new'				value='$item_info->price'			size=2 readonly style='color:#777777'></td>
-				<td style='width:5%'><input type=checkbox name='cms_item::$rand_id::null::new'						value='delete'	onclick='if (confirm(\"確定要刪除這個項目？\")) { document.getElementById(\"item_row_$rand_id\").style.display=\"none\"; calculate(); } else { this.checked=false; }'></td>
+				<td width='100'>$item_info->item_id</td>
+				<td width='430'>$item_info->name</td>
+				<td width='80'><input style='min-width: 60px' class=form-control number type=text name='cms_item::$rand_id::quantity::new'			value='1'							size=2 nextinput=cms_item::$rand_id::price::new		onblur='calculate_item(\"$rand_id\", \"::new\");'></td>
+				<td width='80'><input style='min-width: 60px' class=form-control number type=text name='cms_item::$rand_id::price_original::new'		value='$item_info->price'			size=2 nextinput=cms_item::$rand_id::price::new></td>
+				<td width='80'><input style='min-width: 60px' class=form-control number type=text name='cms_item::$rand_id::price::new'				value='$item_info->price'			size=2 nextinput=input_pulldownmenu_add_item_id		onblur='calculate_item(\"$rand_id\", \"::new\");'></td>
+				<td width='80'><input style='min-width: 60px' class=form-control number type=text name='cms_item::$rand_id::amount::new'				value='$item_info->price'			size=2 readonly style='color:#777777'></td>
+				<td width='40px'><input type=checkbox name='cms_item::$rand_id::null::new'						value='delete'	onclick='if (confirm(\"確定要刪除這個項目？\")) { document.getElementById(\"item_row_$rand_id\").style.display=\"none\"; calculate(); } else { this.checked=false; }'></td>
 			</tr>
 		</table>
 		<script>
